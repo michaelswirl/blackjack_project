b3b09a53e23c09e9ac050d17792dd4fea514455f
Michael Curley <michaelcurley@Michaels-MBP.fios-router.home>
2023-07-25 08:40:51 -0400

    Fixed card suit and rank to be declared before the constructor, and added card title

--- card.py
+++ card.py
@@ -1,8 +1,11 @@
 class Card:
     POSSIBLE_RANK = ['A','1','2','3','4','5','6','7','8','9','10','J','Q','K']
     POSSIBLE_SUIT = ['Clubs','Spades','Hearts','Diamonds']
+    suit = ''
+    rank = ''
+    card_title = ''
     def __init__(self,rank_index,suit_index):
-        self.rank = POSSIBLE_RANK[rank_index]
-        self.suit = POSSIBLE_SUIT[suit_index]
-    card_title = (self.suit, self.rank)
+        self.rank = self.POSSIBLE_RANK[rank_index]
+        self.suit = self.POSSIBLE_SUIT[suit_index]
+        self.card_title = [self.suit, self.rank]
    
--- deck.py
+++ deck.py
@@ -1,5 +1,18 @@
-import card.py
+from card import Card
 
-class Deck():
-    number_of_cards = 52
-    
+class Deck(Card):
+    cards = []
+    card_titles = []
+    def __init__(self):
+        suit_counter = 0
+        for suit in Card.POSSIBLE_SUIT:
+            rank_counter = 0
+            for rank in Card.POSSIBLE_RANK:
+                card = Card(suit == Card.POSSIBLE_SUIT[suit_counter], rank == Card.POSSIBLE_RANK[rank_counter])
+                self.cards.append(card)
+                self.card_titles.append(card.card_title)
+
+
+deck = Deck()
+
+print(deck.card_titles)
