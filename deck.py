12ade4a959dafbd066a990055e57d784d286e07d
Michael Curley <michaelcurley@Michaels-MBP.fios-router.home>
2023-07-25 20:00:41 -0400

    Fixed issue with mutable class variable in deck class that led to incorrect shoe calculations

--- deck.py
+++ deck.py
@@ -2,10 +2,10 @@ from card import Card
 import random
 
 class Deck():
-    current_card = None
-    count = 0
-    cards = []
     def __init__(self):
+        self.current_card = None
+        self.count = None
+        self.cards = []
         self.suit_counter = 0
         for suit in Card.POSSIBLE_SUIT:
             self.rank_counter = 0
@@ -14,6 +14,7 @@ class Deck():
                 self.cards.append(card)
                 self.rank_counter += 1
             self.suit_counter += 1
+        self.count = len(self.cards)
     def shuffle(self):
         random.seed = 46
         random.shuffle(self.cards)
@@ -24,10 +25,11 @@ class Deck():
         return titles
     def deal(self):
         self.current_card = self.cards.pop(0)
+        self.count = len(self.cards)
     
 deck = Deck()
 print("Number of Cards in Deck:")
-print(len(deck.cards))
+print(deck.count)
 print("Unshuffled Deck:")
 print(deck.display())
 print("Shuffled Deck:")
--- shoe.py
+++ shoe.py
@@ -2,13 +2,22 @@ from card import Card
 from deck import Deck
 
 class Shoe(Deck):
-    decks = []
     def __init__(self,number_of_decks):
-        for i in range(1,number_of_decks):
-            deck = Deck()
-            self.decks.append(deck)
+        self.decks = []
+        self.cards = []
+        for i in range(number_of_decks):
+            self.deck = Deck()
+            self.decks.append(self.deck)
+            self.cards += self.deck.cards
+            self.count = len(self.cards)
+    def add_deck(self):
+        deck = Deck()
+        self.decks.append(deck)
 
 shoe = Shoe(8)
 
-print(shoe.cards)
+print(shoe.count)
+print(len(shoe.cards))
 print(shoe.display())
+print(len(shoe.decks))
+print(shoe.decks)
